98f99dddafb3745cd482650ce99deb05991cac39
Florian Freund <florian88freund@gmail.com>
2022-06-10 18:45:54 +0200

    Add non-owned variant to allow chained I2C devices

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 extern crate byteorder;
 extern crate embedded_hal as hal;
 
-use byteorder::{ByteOrder, BigEndian};
+use byteorder::{BigEndian, ByteOrder};
 use hal::blocking::i2c;
 
 pub const INA219_ADDR: u8 = 0x41;
@@ -14,56 +14,88 @@ enum Register {
     BusVoltage = 0x02,
     Power = 0x03,
     Current = 0x04,
-    Calibration = 0x05
+    Calibration = 0x05,
 }
 
-pub struct INA219<I2C> {
-    i2c: I2C,
-    address: u8
+pub struct INA219NonOwned<I2C> {
+    address: u8,
+    _marker: core::marker::PhantomData<I2C>,
 }
 
-impl<I2C, E> INA219<I2C>
-    where I2C: i2c::Write<Error = E> + i2c::Read<Error = E> {
-    pub fn new(i2c: I2C, address: u8) -> INA219<I2C> {
-        INA219 {
-            i2c,
-            address
+impl<I2C, E> INA219NonOwned<I2C>
+where
+    I2C: i2c::Write<Error = E> + i2c::Read<Error = E>,
+{
+    pub fn new(address: u8) -> INA219NonOwned<I2C> {
+        INA219NonOwned {
+            address,
+            _marker: core::marker::PhantomData,
         }
     }
 
-    pub fn calibrate(&mut self, value: u16) -> Result<(), E> {
-        self.i2c.write(self.address, &[
-            Register::Calibration as u8,
-            (value >> 8) as u8,
-            value as u8
-        ])?;
+    pub fn calibrate(&self, i2c: &mut I2C, value: u16) -> Result<(), E> {
+        i2c.write(
+            self.address,
+            &[Register::Calibration as u8, (value >> 8) as u8, value as u8],
+        )?;
         Ok(())
     }
 
-    pub fn shunt_voltage(&mut self) -> Result<i16, E> {
-        let value = self.read(Register::ShuntVoltage)?;
+    pub fn shunt_voltage(&mut self, i2c: &mut I2C) -> Result<i16, E> {
+        let value = self.read(i2c, Register::ShuntVoltage)?;
         Ok(value as i16)
     }
 
-    pub fn voltage(&mut self) -> Result<u16, E> {
-        let value = self.read(Register::BusVoltage)?;
+    pub fn voltage(&mut self, i2c: &mut I2C) -> Result<u16, E> {
+        let value = self.read(i2c, Register::BusVoltage)?;
         Ok((value >> 3) * 4)
     }
 
-    pub fn power(&mut self) -> Result<i16, E> {
-        let value = self.read(Register::Power)?;
+    pub fn power(&mut self, i2c: &mut I2C) -> Result<i16, E> {
+        let value = self.read(i2c, Register::Power)?;
         Ok(value as i16)
+    }
+
+    pub fn current(&mut self, i2c: &mut I2C) -> Result<i16, E> {
+        let value = self.read(i2c, Register::Current)?;
+        Ok(value as i16)
+    }
+
+    fn read(&mut self, i2c: &mut I2C, register: Register) -> Result<u16, E> {
+        let mut buf: [u8; 2] = [0x00; 2];
+        i2c.write(self.address, &[register as u8])?;
+        i2c.read(self.address, &mut buf)?;
+        Ok(BigEndian::read_u16(&buf))
+    }
+}
+
+pub struct INA219<I2C>(I2C, INA219NonOwned<I2C>);
+
+impl<I2C, E> INA219<I2C>
+where
+    I2C: i2c::Write<Error = E> + i2c::Read<Error = E>,
+{
+    pub fn new(i2c: I2C, address: u8) -> INA219<I2C> {
+        INA219(i2c, INA219NonOwned::new(address))
+    }
+
+    pub fn calibrate(&mut self, value: u16) -> Result<(), E> {
+        self.1.calibrate(&mut self.0, value)
+    }
+
+    pub fn shunt_voltage(&mut self) -> Result<i16, E> {
+        self.1.shunt_voltage(&mut self.0)
+    }
+
+    pub fn voltage(&mut self) -> Result<u16, E> {
+        self.1.voltage(&mut self.0)
+    }
+
+    pub fn power(&mut self) -> Result<i16, E> {
+        self.1.power(&mut self.0)
     }
 
     pub fn current(&mut self) -> Result<i16, E> {
-        let value = self.read(Register::Current)?;
-        Ok(value as i16)
-    }
-
-    fn read(&mut self, register: Register) -> Result<u16, E> {
-        let mut buf: [u8; 2] = [0x00; 2];
-        self.i2c.write(self.address, &[register as u8])?;
-        self.i2c.read(self.address, &mut buf)?;
-        Ok(BigEndian::read_u16(&buf))
+        self.1.current(&mut self.0)
     }
 }
